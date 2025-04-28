613615ea13946626ed5f3b0ec17ef17c9c6aa3ba
y6714453 <y6714453@gmail.com>
2025-04-28 13:18:58 +0300

    Update bitcoin.php

--- bitcoin.php
+++ bitcoin.php
@@ -1,5 +1,15 @@
 <?php
-$response = @file_get_contents('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT');
+function getApiData($url) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_TIMEOUT, 5); // ממתין עד 5 שניות
+    $response = curl_exec($ch);
+    curl_close($ch);
+    return $response;
+}
+
+$response = getApiData('https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT');
 
 if ($response !== false) {
     $data = json_decode($response, true);
