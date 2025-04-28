8aa8b11d99d493c7a679c88b0a1e6ce2e57c30e5
y6714453 <y6714453@gmail.com>
2025-04-28 13:33:02 +0300

    Update bitcoin.php

--- bitcoin.php
+++ bitcoin.php
@@ -9,12 +9,12 @@ function getApiData($url) {
     return $response;
 }
 
-$response = getApiData('https://api.coincap.io/v2/assets/bitcoin');
+$response = getApiData('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd');
 
 if ($response !== false) {
     $data = json_decode($response, true);
-    if (isset($data['data']['priceUsd'])) {
-        $price = number_format($data['data']['priceUsd'], 2);
+    if (isset($data['bitcoin']['usd'])) {
+        $price = number_format($data['bitcoin']['usd'], 2);
         echo "הביטקוין עומד כעת על $price דולר.";
     } else {
         echo "המידע על הביטקוין אינו זמין כרגע, נסו שוב מאוחר יותר.";
