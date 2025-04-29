dcd2dc5c0a84620ed2784c0edb873b5130d3f94a
y6714453 <y6714453@gmail.com>
2025-04-29 22:10:21 +0300

    Update sp500.php

--- sp500.php
+++ sp500.php
@@ -4,7 +4,10 @@ function getApiData($url) {
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-    curl_setopt($ch, CURLOPT_ENCODING, ''); // תמיכה ב-gzip
+    curl_setopt($ch, CURLOPT_ENCODING, '');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36'
+    ]);
     $response = curl_exec($ch);
     curl_close($ch);
     return $response;
@@ -14,7 +17,7 @@ $url = "https://query1.finance.yahoo.com/v8/finance/chart/^GSPC";
 $response = getApiData($url);
 $data = json_decode($response, true);
 
-if (isset($data['chart']['result']) && is_array($data['chart']['result']) && isset($data['chart']['result'][0]['meta']['regularMarketPrice'])) {
+if (isset($data['chart']['result'][0]['meta']['regularMarketPrice'])) {
     $price = $data['chart']['result'][0]['meta']['regularMarketPrice'];
     echo "מחיר מדד S&P 500 כעת הוא: " . number_format($price, 2) . " דולר.";
 } else {
