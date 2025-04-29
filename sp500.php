e6f9c5d170ac19791f91277cf5794953d712b9e8
y6714453 <y6714453@gmail.com>
2025-04-29 22:08:00 +0300

    Update sp500.php

--- sp500.php
+++ sp500.php
@@ -4,7 +4,7 @@ function getApiData($url) {
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_TIMEOUT, 5);
-    curl_setopt($ch, CURLOPT_ENCODING, ''); // תומך בדחיסות gzip וכו'
+    curl_setopt($ch, CURLOPT_ENCODING, ''); // תמיכה ב-gzip
     $response = curl_exec($ch);
     curl_close($ch);
     return $response;
@@ -14,10 +14,10 @@ $url = "https://query1.finance.yahoo.com/v8/finance/chart/^GSPC";
 $response = getApiData($url);
 $data = json_decode($response, true);
 
-if (isset($data['chart']['result'][0]['meta']['regularMarketPrice'])) {
+if (isset($data['chart']['result']) && is_array($data['chart']['result']) && isset($data['chart']['result'][0]['meta']['regularMarketPrice'])) {
     $price = $data['chart']['result'][0]['meta']['regularMarketPrice'];
     echo "מחיר מדד S&P 500 כעת הוא: " . number_format($price, 2) . " דולר.";
 } else {
-    echo "שגיאה: לא נמצא regularMarketPrice";
+    echo "שגיאה: לא נמצא מחיר עדכני למדד.";
 }
 ?>
