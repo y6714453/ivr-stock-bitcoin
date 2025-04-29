a0bbcc4ee0384579a69aa1302c6a362922c7fd09
y6714453 <y6714453@gmail.com>
2025-04-29 12:42:55 +0300

    Update sp500.php

--- sp500.php
+++ sp500.php
@@ -9,22 +9,27 @@ function getApiData($url) {
     return $response;
 }
 
-$response = getApiData('https://query1.finance.yahoo.com/v8/finance/chart/^GSPC');
+// הכנס את המפתח שלך כאן
+$apiKey = "OXe5vDCaDebbABKvEJlYUVF3awqN0e1f";
+$url = "https://api.polygon.io/v2/aggs/ticker/I:SPX/prev?adjusted=true&apiKey=$apiKey";
+
+$response = getApiData($url);
 
 if ($response !== false) {
     $data = json_decode($response, true);
-    if (isset($data['chart']['result'][0]['meta']['regularMarketPrice'])) {
-        $price = number_format((float)$data['chart']['result'][0]['meta']['regularMarketPrice'], 0);
+    if (isset($data['results'][0]['c'])) {
+        $price = number_format((float)$data['results'][0]['c'], 0);
+
         $thousands = floor($price / 1000);
         $rest = $price % 1000;
 
         if ($rest > 0) {
-            echo "האס אנד פי עומד כעת על $thousands אלף ו$rest דולר.";
+            echo "אס אנד פי 500 עומד כעת על $thousands אלף ו$rest דולר.";
         } else {
-            echo "האס אנד פי עומד כעת על $thousands אלף דולר.";
+            echo "אס אנד פי 500 עומד כעת על $thousands אלף דולר.";
         }
     } else {
-        echo "המידע על האס אנד פי אינו זמין כרגע, נסו שוב מאוחר יותר.";
+        echo "המידע על אס אנד פי 500 אינו זמין כרגע, נסו שוב מאוחר יותר.";
     }
 } else {
     echo "התקשורת עם שרת המידע נכשלה.";
