45d04f29bf24d5355afa6b0d8b95645a68ee4e54
y6714453 <y6714453@gmail.com>
2025-05-02 00:57:56 +0300

    Update ethereum.php

--- ethereum.php
+++ ethereum.php
@@ -1,1 +1,11 @@
+<?php
+$response = file_get_contents("https://api.binance.com/api/v3/ticker/price?symbol=ETHUSDT");
+$data = json_decode($response, true);
 
+if (isset($data['price'])) {
+    $price = number_format((float)$data['price'], 0);
+    echo "מחיר האיתריום כעת הוא: $price דולר.";
+} else {
+    echo "המידע על האיתריום אינו זמין כרגע.";
+}
+?>
