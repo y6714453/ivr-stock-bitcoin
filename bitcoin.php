0460c08c64068301e311cfb4627f01a5037bc869
y6714453 <y6714453@gmail.com>
2025-05-02 01:13:18 +0300

    Update bitcoin.php

--- bitcoin.php
+++ bitcoin.php
@@ -3,9 +3,9 @@ $response = file_get_contents("https://api.binance.com/api/v3/ticker/price?symbo
 $data = json_decode($response, true);
 
 if (isset($data['price'])) {
-    $price = number_format((float)$data['price'], 0);
-    echo "מחיר הביטקוין כעת הוא: $price דולר.";
+    $price = (int) $data['price'];
+    echo $price;
 } else {
-    echo "המידע על הביטקוין אינו זמין כרגע.";
+    echo 0;
 }
 ?>
