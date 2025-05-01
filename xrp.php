2c170028552281c1dd23ed4edfaefa5f882ca394
y6714453 <y6714453@gmail.com>
2025-05-02 00:58:38 +0300

    Update xrp.php

--- xrp.php
+++ xrp.php
@@ -1,1 +1,11 @@
+<?php
+$response = file_get_contents("https://api.binance.com/api/v3/ticker/price?symbol=XRPUSDT");
+$data = json_decode($response, true);
 
+if (isset($data['price'])) {
+    $price = number_format((float)$data['price'], 0);
+    echo "מחיר ה XRP כעת הוא: $price דולר.";
+} else {
+    echo "המידע על ה XRP אינו זמין כרגע.";
+}
+?>
