f1d8ed6def7a31081fd22fff2561c4385a80fefc
y6714453 <y6714453@gmail.com>
2025-05-07 16:34:01 +0300

    Create bitcoin_fast.php

---
+++ bitcoin_fast.php
@@ -0,0 +1,11 @@
+<?php
+$response = file_get_contents("https://api.binance.com/api/v3/ticker/price?symbol=BTCUSDT");
+$data = json_decode($response, true);
+
+if (isset($data['price'])) {
+    $price = round((float)$data['price']);
+    echo $price;
+} else {
+    echo "אין נתונים זמינים";
+}
+?>
