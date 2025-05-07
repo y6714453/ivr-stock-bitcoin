b2701f6d793587509d27ad9a28df06f80ed24fec
y6714453 <y6714453@gmail.com>
2025-05-07 16:36:47 +0300

    Update bitcoin_fast.php

--- bitcoin_fast.php
+++ bitcoin_fast.php
@@ -4,7 +4,7 @@ $data = json_decode($response, true);
 
 if (isset($data['price'])) {
     $price = round((float)$data['price']);
-    echo $price;
+    echo $price . " דולר";
 } else {
     echo "אין נתונים זמינים";
 }
